a7240275e9a8bb1ce47590243f3ff7bb8c6d0a99
Berserker19 <jbcloud11@gmail.com>
2024-08-04 14:32:52 -0400

    Add files via upload

---
+++ D277/validate.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const email = document.getElementById("email");
+    const confirmEmail = document.getElementById("confirmEmail");
+
+    confirmEmail.addEventListener("input", function() {
+        if (email.value !== confirmEmail.value) {
+            confirmEmail.setCustomValidity("Email addresses do not match.");
+        } else {
+            confirmEmail.setCustomValidity("");
+        }
+    });
+});
