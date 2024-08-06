aa8e614183e8ecf0f8a2133019954f7261c1e27e
Berserker19 <jbcloud11@gmail.com>
2024-08-06 19:12:13 -0400

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   const email = document.getElementById("email");
   const confirmEmail = document.getElementById("confirmEmail");
+  const form = document.getElementById("contactForm");
 
   confirmEmail.addEventListener("input", function () {
     if (email.value !== confirmEmail.value) {
       confirmEmail.setCustomValidity("Email addresses do not match.");
     } else {
       confirmEmail.setCustomValidity("");
+    }
+  });
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (email.value === confirmEmail.value) {
+      alert("Form submitted successfully!");
+      form.reset();
+    } else {
+      alert("Please make sure the email addresses match.");
     }
   });
 });
